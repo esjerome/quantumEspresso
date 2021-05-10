af393a4239a151889433c046a4f84b9f843e3f0d
ejrodriguezm <ejrodriguezm@guane.uis.edu.co>
2021-05-09 23:17:50 -0500

    archivos de ejecucion para orbitales

---
+++ ag12bic/orbitales/run_pp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=QE
+#SBATCH --partition=guane_24_cores
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --output=qe.out
+#SBATCH --error=qe.erra
+
+
+module load pmix/2.2.2
+module load QuantumExpresso/6.5
+export FI_PROVIDER=tcp
+
+mpirun -n 24 pp.x < ag12bic-scf-pp.in > ag12bic-scf-pp.out
